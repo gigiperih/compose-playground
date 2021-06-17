cc25ef79c7253085aca35fce1dede1db2454da3f
gilangabdulaziz <gilangabdulaziz@gmail.com>
2021-06-17 15:28:00 +0700

    fix stuff

--- app/src/main/java/io/gigiperih/composable/MainActivity.kt
+++ app/src/main/java/io/gigiperih/composable/MainActivity.kt
@@ -32,7 +32,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "Hell sdsdfsdfsf o $name!")
 }
 
 @Preview(showBackground = true)
@@ -45,7 +45,7 @@ fun DefaultPreview() {
                     Text(
                         color = SteelGray,
                         fontWeight = FontWeight.Bold,
-                        text = "gaga sss"
+                        text = "gaga sss sfsdfsf "
                     )
                 }
             }
